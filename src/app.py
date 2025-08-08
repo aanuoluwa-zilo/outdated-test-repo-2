5c589d03e5c19ed1339c68df461154dac05eb909
aanuoluwa-zilo <168453615+aanuoluwa-zilo@users.noreply.github.com>
2025-08-08 12:07:49 +0100

    Add new features to simulate outdated state

--- src/app.py
+++ src/app.py
@@ -1,17 +1,28 @@
 class App:
     def __init__(self):
-        self.name = 'Outdated Test App 2'
-        self.version = '1.0.0'
+        self.name = 'Outdated Test App 2 - UPDATED'
+        self.version = '2.0.0'
+        self.features = ['basic', 'advanced']
     
     def run(self):
         print(f'Running {self.name} v{self.version}')
+        self.new_feature()
         return True
     
     def get_info(self):
         return {
             'name': self.name,
-            'version': self.version
+            'version': self.version,
+            'features': self.features
         }
+    
+    def new_feature(self):
+        print('New feature added to simulate outdated state')
+        return 'feature_added'
+    
+    def process_data(self, data):
+        # New method to process data
+        return [item * 2 for item in data]
 
 def main():
     app = App()
